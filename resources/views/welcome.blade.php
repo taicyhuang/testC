b23ed73ca1b92fe2efc65ad021c6d22330c95113
webber_huang <taicy.huang@gmail.com>
2022-06-13 14:31:24 +0800

    flex 圖片會被吃掉

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    {{--
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> --}}
     <title>一站式登入</title>
 
     <!-- Fonts -->
@@ -18,28 +21,51 @@
 </head>
 
 <body>
-
     <div class="container">
-        <img style="max-width:40%; max-height:100%" src="{{  asset('/storage/images/cfright.png')}}" alt="">
-        <div>Welcome!</div>
-        <div class="">CLICKFORCE</div>
-        <div class="login_form_outer1">
+        <div class="d-flex flex-row-reverse">
+            <div class="p-2">
+                <div class="logo">
+                    <div class="p-2 welcome">Welcome!</div>
+                    <img class="d-block mx-auto" src="https://cerp.clickforce.asia/image/logo_english 2.png">
+                </div>
+                <div class="p-6">
+                    <div class="login_form_outer">
+                        <div class="login_form">
+                            <form method="POST" id="form" action="https://cerp.clickforce.asia/login">
+                                <input type="hidden" name="_token" value="ozMruHzgWG2MP2Rjb0PneYkJXJLilZu6cAeVPD0x">
+                                <div class="mb-3">
+                                    <input id="email" type="email" class="form-control form_style  " name="email"
+                                        required="required" value="" autocomplete="email" autofocus="" placeholder="帳號">
+                                </div>
+                                <div class="mb-3 position-relative">
+                                    <input id="password" type="password" class="form-control form_style "
+                                        name="password" required="" autocomplete="current-password" placeholder="密碼">
+                                    <span class="show_pw fas fa-eye"></span>
+                                </div>
+                                <div class="d-flex justify-content-between" style="padding-top: 20px;">
+                                    <button type="button" class="form-control button"
+                                        onclick="go_forget_url()">忘記密碼</button>
+                                    <button type="submit" class="form-control button" id="login-btn">登入系統</button>
+                                </div>
+                            </form>
+                        </div>
 
-            <div class="">
-                <div>
-                    <label for="">帳號</label><input type="text">
+                    </div>
                 </div>
-                <div>
-                    <label for="">密碼</label><input type="text">
+                <div class="d-flex flex-row-reverse">
+                    <div class="p-2"><img src="{{ asset('/storage/images/HOLMES_ssp.png')}}" alt=""></div>
+                    <div class="p-2"><img src="{{ asset('/storage/images/HOLMES_ADS-0114.png')}}" alt=""></div>
+                    <div class="p-2"><img src="{{ asset('/storage/images/HOLMES_DATA.png')}}" alt=""></div>
                 </div>
-                <div>
-                    <button>登入</button>
-                    <button>取消</button>
-                </div>
+                {{-- <div class="p-8">
+                    <div class="p-2"> <img src="{{ asset('/storage/images/HOLMES_ssp 2.png')}}" alt=""></div>
+                    <div class="p-2"> <img src="{{ asset('/storage/images/HOLMES_ssp 2.png')}}" alt=""></div>
+                    <div class="p-2"> <img src="{{ asset('/storage/images/HOLMES_ssp 2.png')}}" alt=""></div>
 
+                </div> --}}
             </div>
-        </div>
 
+        </div>
     </div>
 </body>
 
@@ -62,8 +88,8 @@
         /* background-position: center; */
         /* background-size: 75%; */
         /* background-size: contain; */
-        padding-top: 50%;
-        margin-left: 70%;
+        padding-top: 50px;
+        margin-left: 400px;
         border-top-left-radius: 5%;
         border-top-right-radius: 5%;
         border-bottom-right-radius: 5%;
@@ -80,13 +106,18 @@
         /* /* margin: 10px; */
         /* display: block; */
         /* /* padding-top: 20%; */
-        margin-left: 60% */
+        margin-right: 100px;
     }
 
     .welcome {
-        /* margin-left: 80%; */
-        margin-top: 20%;
-        border-radius: 5%;
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 72px;
+        /* line-height: 140%; */
+        align-items: center;
+        text-align: center;
+        color: #4D4D4D;
     }
 </style>
 
